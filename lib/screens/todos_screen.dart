5405447689d6b80e421832a73f738a42588cfb58
Huzaifah and Hakim <m.huzaifah1998@gmail.com>
2020-04-21 22:13:50 +0800

    Update todos_screen.dart

--- lib/screens/todos_screen.dart
+++ lib/screens/todos_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'tasks_screen.dart';
 
 class TodoListScreen extends StatefulWidget {
   @override
@@ -6,6 +7,27 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
+  // void _navigate(index) async {
+  //   final GroupMember _evaluator;
+  //   final List _data;
+
+  //   TodoListScreen(this._evaluator, this._data);
+
+  //   final returnData = await Navigator.push(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (context) => TaskListScreen(
+  //         Assessment.copy(widget._data[index]),
+  //       ),
+  //     ),
+  //   );
+
+  //   if (returnData != null) {
+  //     setState(() => widget._data[index] = returnData);
+  //   }
+  // }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +42,7 @@ class _TodoListScreenState extends State<TodoListScreen> {
         itemBuilder: (context, index) => ListTile(
           title: Text('Todo Title'),
           subtitle: Text('How many task'),
+          onTap: () => TaskListScreen(),
         ),
       ),
     );
